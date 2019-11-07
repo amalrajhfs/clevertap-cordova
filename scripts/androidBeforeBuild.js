d258e58b5a5e51f3bd75213c8fd158bd6bb838ab
Amal Raj <amal@headfitted.com>
2019-11-07 13:44:53 +0530

    Update androidBeforeBuild.js

--- scripts/androidBeforeBuild.js
+++ scripts/androidBeforeBuild.js
@@ -27,7 +27,7 @@ module.exports = function(context) {
               if (err) {
                   throw new Error('Unable to write into build.gradle: ' + err);
               }
-              console.log('AMAL: Updated gradle file');
+              console.info('AMAL: Updated gradle file');
             });
           }
         });
