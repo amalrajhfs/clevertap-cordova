a13d8370e5bcd16977e4cbfc268f6bb66faa18b7
Amal Raj <amal@headfitted.com>
2019-11-07 12:36:39 +0530

    Create androidBeforeBuild.js

---
+++ scripts/androidBeforeBuild.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+module.exports = function(context) {
+
+  var fs = context.requireCordovaModule('fs'),
+    path = context.requireCordovaModule('path');
+
+  var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+
+  var gradleFile = path.join(platformRoot, 'build.gradle');
+
+  if (fs.existsSync(gradleFile)) {
+    try {
+        fs.readFile(gradleFile, 'utf8', function (err,data) {
+          if (err) {
+            throw new Error('Unable to find build.gradle: ' + err);
+          }
+
+          var oldGradleDep = 'com.android.tools.build:gradle:3.3.0';
+		  var newGradleDep = 'com.android.tools.build:gradle:3.3.1';
+        
+          if (data.indexOf(oldGradleDep) == -1) {
+
+            var result = data.replace(oldGradleDep, newGradleDep);
+
+            fs.writeFile(gradleFile, result, 'utf8', function (err) {
+              if (err) {
+                  throw new Error('Unable to write into build.gradle: ' + err);
+              }
+            });
+          }
+        });
+    } catch(err) {
+        process.stdout.write(err);
+    }
+  }
+}
