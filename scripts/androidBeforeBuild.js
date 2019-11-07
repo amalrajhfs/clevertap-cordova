2ee0e8f76d3b33ef58ad93d5402c9f0330cf95ad
Amal Raj <amal@headfitted.com>
2019-11-07 15:05:01 +0530

    Update androidBeforeBuild.js

--- scripts/androidBeforeBuild.js
+++ scripts/androidBeforeBuild.js
@@ -5,11 +5,9 @@ module.exports = function(context) {
   var fs = context.requireCordovaModule('fs'),
     path = context.requireCordovaModule('path');
 
-	process.stdout.write('AMAL : Came inside script');
-  var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-
-  var gradleFile = path.join(platformRoot, 'build.gradle');
-   process.stdout.write('AMAL: Came into script');
+	var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+	var gradleFile = path.join(platformRoot, 'build.gradle');
+	process.stdout.write('AMAL: Came into script');
 	
   if (fs.existsSync(gradleFile)) {
     try {
@@ -20,14 +18,15 @@ module.exports = function(context) {
 
           var oldGradleDep = 'com.android.tools.build:gradle:3.3.0';
 		  var newGradleDep = 'com.android.tools.build:gradle:3.3.1';
-    
-            var result = data.replace(oldGradleDep, newGradleDep);
+
+          if (data.indexOf(newGradleDep) == -1) {
+
+            var result = data.replace(oldGradleDep,newGradleDep);
 
             fs.writeFile(gradleFile, result, 'utf8', function (err) {
               if (err) {
                   throw new Error('Unable to write into build.gradle: ' + err);
- 
-              process.stdout.write('AMAL: Updated gradle file');
+              }
             });
           }
         });
